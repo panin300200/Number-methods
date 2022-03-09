78a9e94ffa3903fbf5b22e89121d0ec9aa714e0a
panin300200 <panin300200@gmail.com>
2022-03-09 23:03:01 +0700

    Add ignore file

---
+++ InterpolationSplain.hpp
@@ -0,0 +1,22 @@
+#include <fstream>
+#include <vector>
+#include <tuple>
+
+
+using Point = std::tuple<double, double, double>;
+namespace laba1 { class InterpolationSplain; }
+
+
+class InterpolationSplain
+{
+	public:
+
+		InterpolationSplain(std::vector<Point> const& a = {}, std::vector<double> const& b = {})
+		{}
+
+
+	private:
+	// fields
+	// std::vector<double>
+	// std::vector<Point>
+};
